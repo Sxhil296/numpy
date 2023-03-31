0788588275687744f710b2a34b931114e909d994
Sahil <leviack629@gmail.com>
2023-03-31 22:42:23 +0530

    numpy basics

--- main.py
+++ main.py
@@ -13,15 +13,19 @@ print(type(arr1))
 #0-D arrays
 arrD = np.array(42)
 print(arrD)
+print(arrD.ndim) #to check the number of dimensions
 
 #1-D arrays
 arr1D = np.array([2, 3, 4, 5])
 print(arr1D)
+print(arr1D.ndim) #to check the number of dimensions
 
 #2-D arrays
 arr2D = np.array([[1, 2, 3], [4, 5, 6]])
 print(arr2D)
+print(arr2D.ndim) #to check the number of dimensions
 
 #3-D arrays
 arr3D = np.array([[[1,2,3], [4,5,6], [7,8,9]]])
 print(arr3D)
+print(arr3D.ndim) #to check the number of dimensions
