6ff01bbf3a143f2490ba8a08f427cd6d0f8fff40
Sahil <leviack629@gmail.com>
2023-04-02 00:10:35 +0530

    datatype

--- datatype.py
+++ datatype.py
@@ -17,3 +17,19 @@ print(arr1.dtype)
 arr4=np.array([1,2,3,4], dtype='i4')
 print(arr4)
 print(arr4.dtype)
+
+#a non integer string can not be converted to a integer.
+
+
+#converting datatypes on existing arrays
+arrold=np.array([1,2,3,4,5])
+print(arrold.dtype) #int64
+arrnew=arrold.astype('i')
+print(arrnew.dtype) #int32
+
+arrfloat=np.array([1.1, 2.2, 3.3])
+print(arrfloat.dtype) #float64
+newarr=arrfloat.astype(int)
+print(newarr.dtype) #int64
+boolarr=newarr.astype(bool)
+print(boolarr.dtype) #bool
