7ab59024fa1bdf561802f10648f974445e482b2c
Sahil <leviack629@gmail.com>
2023-04-01 23:48:54 +0530

    slicing

---
+++ slicing.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(arr)
+print(arr[1:5]) #prints elements from index 1 to index 5 (excluding index 5)
+print(arr[4:]) #from index 4 to end
+print(arr[:4]) #from index 0 to index 4 (doesn't include index 4)
+print(arr[-3:-1])
+
+
+#using the step value
+print(arr[1:5:2])
+print(arr[::2])
