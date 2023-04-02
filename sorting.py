d619a9db2788427405bb36a2a06ea10c4094465a
Sahil <leviack629@gmail.com>
2023-04-03 02:17:31 +0530

    sorting

---
+++ sorting.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+array=np.array([3,2,0,11,5,1])
+print(np.sort(array))
+
+arr = np.array(['banana', 'cherry', 'apple'])
+print(np.sort(arr))
+
+arrBool = np.array([True, False, True])
+print(np.sort(arrBool))
+
+#sorting 2D arrays
+a=np.array([[3,2,4], [5,0,1]])
+print(np.sort(a))
