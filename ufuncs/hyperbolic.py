d8392384dce8d8df6f2a63932547327e1df783f8
Sahil <leviack629@gmail.com>
2023-04-08 23:42:48 +0530

    hyperbolic functions

---
+++ ufuncs/hyperbolic.py
@@ -0,0 +1,15 @@
+import numpy as np
+x=np.sinh(np.pi/2)
+print(x)
+
+arr= np.array([np.pi/2, np.pi/3, np.pi/4, np.pi/5])
+y=np.cosh(arr)
+print(y)
+
+#finding angles
+a=np.arcsin(1)
+print(a)
+
+arr1=np.array([0.2, 0.1, 0.5])
+y=np.arctanh(arr1)
+print(y)
